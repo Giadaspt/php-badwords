f01091eb4311cdf6061f797ee9077e79b4a8c80d
Giadaspt <saputo.giada@gmail.com>
2021-12-17 13:08:35 +0100

    primo commit php

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php 
+
+  $titolo = 'Bonsenso pratico';
+  $paragrafo = 'Quanno de notte sparsero la voce <br/>
+  che un Fantasma girava sur castello <br/>
+  tutta la Folla corse e, ner vedello, <br/>
+  cascò in ginocchio co’ le braccia in croce. <br/>
+  Ma un Vecchio restò in piedi, e francamente <br/>
+  voleva dije che nun c’era gnente. <br/>
+  Poi ripensò: - Sarebbe una pazzia. <br/>
+  Io, senza dubbio, vedo ch’è un lenzolo: <br/>
+  ma più che di’ la verità da solo, <br/>
+  preferisco sbajamme in compagnia. <br/>
+  Dunque è un Fantasma, senza discussione. - <br/>
+  E pure lui se mise a pecorone.';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h2> <?php echo  $titolo ?> </h2>
+  <p> <?php echo $paragrafo ?></p>
+  <p> La poesia è lunga <?php echo strlen($paragrafo) ?> caratteri </p>
+  
+</body>
+</html>
+
